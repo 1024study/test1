f0a5364dac95b3b587ae6b6bbb0deaa2b7a1f341
miao-git <10459079@qq.com>
2022-08-17 11:15:28 +0800

    第八次提交--hot-fix分支修改另一个文件，等会合并到master分支，看是否会合并冲突

---
+++ src/main/java/com/study1024/GitTest.java
@@ -0,0 +1,12 @@
+package com.study1024;
+
+/**
+ * @author: 1024study
+ * @create: 2022-08-17 11:13
+ * @description: TODO
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hot-fix分支修改文件");
+    }
+}
