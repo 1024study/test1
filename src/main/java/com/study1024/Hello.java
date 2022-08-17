294700866066cb81173f6cf74e405fa515484420
miao-git <10459079@qq.com>
2022-08-17 11:20:51 +0800

    --ffff

--- src/main/java/com/study1024/Hello.java
+++ src/main/java/com/study1024/Hello.java
@@ -7,6 +7,7 @@ package com.study1024;
  **/
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("fdsafsadfdsafdsaf");
         System.out.println("Hello Git!!");
         System.out.println("Hello Git2!!");
         System.out.println("Hello Git3!!");
