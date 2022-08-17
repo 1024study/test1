7e6603a78691e45ecbb4a439dc3754f34efe0db4
miao-git <10459079@qq.com>
2022-08-17 10:00:51 +0800

    提交日志信息--第一次提交

---
+++ src/main/java/com/study1024/Hello.java
@@ -0,0 +1,12 @@
+package com.study1024;
+
+/**
+ * @author: 1024study
+ * @create: 2022-08-17 09:50
+ * @description: TODO
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
